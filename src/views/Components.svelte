730695c36bc89f0facc206c1a013b7763af9dbf1
itorr-i7 <592668024@qq.com>
2024-04-26 09:51:15 +0800

    ui-btn 样式冲突

--- src/views/Components.svelte
+++ src/views/Components.svelte
@@ -12,7 +12,8 @@
     <div class="ui-selects">
         <div class="option-item">选项1</div>
         <div class="option-item">选项选项2</div>
-        <div class="option-item">选项选项选项选项选项3</div>
+        <div class="option-item" data-selected="true">选项选项选项选项选项3</div>
+        <div class="option-item" data-selected="true" data-completed="true">选项选项选项选项选项3</div>
         <div class="option-item">选项选项选项4</div>
     </div>
     <div>
